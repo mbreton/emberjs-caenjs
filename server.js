2b35a5334be24cd538be46ebe6a41ce0df72badd
mbreton <mbreton@xebia.fr>
2013-09-12 21:13:30 +0200

    start

--- server.js
+++ server.js
@@ -9,15 +9,17 @@ BEER_TOKEN = "feffbbabf08971e749c7f9dd585e95fb6f754f88d15b6ed5b354eec41ad8e19f";
 app.get('/beers/:page',function(req, res){
     var page = req.params.page || 0;
 
-    request.get({url:"http://api.openbeerdatabase.com/v1/beers.json?per_page=10&page="+page+"&token="+BEER_TOKEN, json:true}, function(err, resp, data){
-        res.send(data.beers);
+    request.get({url:"http://api.openbeerdatabase.com/v1/beers.json?per_page=20&page="+page+"&token="+BEER_TOKEN, json:true}, function(err, resp, data){
+        var result = data ? data.beers : "";
+        res.send(result);
     });
 });
 
 app.get('/beer/:id',function(req, res){
     var id = +req.params.id;
     request.get({url:"http://api.openbeerdatabase.com/v1/beers/" +id+ ".json?token="+BEER_TOKEN, json:true}, function(err, resp, data){
-        res.send(data);
+        var result = data ? data.beers : "";
+        res.send(result);
     });
 });
 
