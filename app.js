e6ce0724d85012bab1e2d3087f8eaf7c349de765
mbreton <mbreton@xebia.fr>
2013-09-15 20:45:45 +0200

    Cleaned, and commented

--- app.js
+++ app.js
@@ -1,50 +1,66 @@
 // ===================
 // Application
 // ===================
-
-App = Ember.Application.create({
-    LOG_VERSION: true,
-    LOG_TRANSITIONS: true
-});
+App = Ember.Application.create();
 
 // ===================
 // Routes
 // ===================
 
+// définition des différents états de l'application
 App.Router.map(function () {
-    this.resource('beers', function () {
-        this.route('beer', {path: '/:id'})
+    this.resource('beers',function(){
+        this.route('beer',{path:'/:id'})
     });
+    this.route('about')
 });
 
+// chargement des données pour l'état 'beers'
 App.BeersRoute = Em.Route.extend({
-    model: function () {
-        return App.Beer.findAll();
+    model:function(){
+        return App.Beer.findAll(1);
     }
 });
 
+
+// chargement des données pour l'état 'beers/:id'
 App.BeersBeerRoute = Em.Route.extend({
-    model: function (url) {
+    model:function(url){
         return App.Beer.find(url.id);
+    }
+});
+
+// Non vue pendant la présentation :
+// Cette route est celle déclencher quand on essaie d'accéder sur '/'.
+// Ici on déclenche une rediction automatique sur 'beers'
+App.IndexRoute = Em.Route.extend({
+    redirect:function(){
+        this.transitionTo('beers');
     }
 });
 
 // ===================
 // Models
 // ===================
-
 App.Beer = Em.Object.extend({
     name: "",
     imageUrl: "",
     description: "",
     created_at: "",
-    updated_at: ""
+    updated_at: "",
+    brewery: "",
+    short_desc: function () {
+        return this.get('description').substring(0, 40) + "...";
+    }.property('description')
 });
 
+// Définition des méthodes de chargement des données à base de promesse
+// (Note : Ember.js gère très bien les promesses, il n'y a pas besoin
+// de les gérer nous même, il se charge de la résoudre pour nous.)
 App.Beer.reopenClass({
     findAll: function (page) {
-        return $.get('/beers/'+page).then(function (json) {
-            return _.map(json, function (jsonBeer) {
+        return $.get('/beers/' + page).then(function (json) {
+            return json.map(function (jsonBeer) {
                 return App.Beer.create(jsonBeer);
             });
         });
@@ -56,16 +72,28 @@ App.Beer.reopenClass({
     }
 });
 
+
 // ===================
 // Controllers
 // ===================
+App.ApplicationController = Em.Controller.extend({
+    init: function () {
+        this.set('name', localStorage.name);
+    },
+
+    actions: {
+        saveName: function (name) {
+            localStorage.name = name;
+        }
+    }
+});
 
 App.BeersController = Em.ArrayController.extend({
     page: 1,
     loadingMoreBeer:false,
     noMoreBeer:false,
 
-    findNextBeers: function () {
+    findNextBeers:function(){
         if (this.noMoreBeer || this.loadingMoreBeer) return;
         this.loadingMoreBeer = true;
         this.page++;
@@ -78,30 +106,19 @@ App.BeersController = Em.ArrayController.extend({
     }
 });
 
-App.ApplicationController = Em.Controller.extend({
-    needs: ['beers'],
-
-    nbBeers: Em.computed.oneWay('controllers.beers.model.length')
-});
-
 // ===================
 // Views & Helpers
 // ===================
-
-Ember.Handlebars.registerBoundHelper('date', function (value) {
-    return moment(value, 'YYYY-MM-DDTHH:mm:ss').calendar();
-});
-
 App.BeersView = Em.View.extend({
-   didInsertElement:function(){
-       var view = this;
-       var $list = this.$("#list");
+    didInsertElement:function(){
+        var view = this;
+        var $list = this.$("#list");
 
-       $list.scroll(function () {
+        $list.scroll(function () {
            var offset = $('.beer-item:last').offset();
            if (offset.top - $list.height() <= 150) {
                view.get('controller').findNextBeers();
            }
-       });
-   }
+        });
+    }
 });
---
+++ img/beers/Evil Dead Red.php
@@ -0,0 +1,534 @@
+<!DOCTYPE html>
+<html id="XenForo" lang="en-US" dir="LTR" class="Public LoggedOut NoSidebar" xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+
+	<meta charset="utf-8" />
+	
+		<base href="http://beeradvocate.com/community/" />
+		<script>
+			var _b = document.getElementsByTagName('base')[0], _bH = "http://beeradvocate.com/community/";
+			if (_b && _b.href != _bH) _b.href = _bH;
+		</script>
+	
+
+	<title>Error | BeerAdvocate Community</title>
+	
+	<noscript><style>.JsOnly { display: none !important; }</style></noscript>
+	<link rel="stylesheet" href="css.php?css=xenforo,form,public&amp;style=2&amp;dir=LTR&amp;d=1378400020" />
+	<link rel="stylesheet" href="css.php?css=login_bar&amp;style=2&amp;dir=LTR&amp;d=1378400020" />
+	
+	
+
+	<script>
+
+	var _gaq = [['_setAccount', 'UA-1066712-1'], ['_trackPageview']];
+	!function(d, t)
+	{
+		var g = d.createElement(t),
+			s = d.getElementsByTagName(t)[0];	
+		g.async = true;
+		g.src = ('https:' == d.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		s.parentNode.insertBefore(g, s);
+	}
+	(document, 'script');
+
+	</script>
+		<script src="js/jquery/jquery-1.5.2.min.js"></script>	
+		
+	<script src="js/xenforo/xenforo.js?_v=1d8d27b3"></script>
+
+	
+	<link rel="alternate" type="application/rss+xml" title="RSS feed for BeerAdvocate Community" href="forums/-/index.rss" />
+	
+	
+
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<meta name="copyright" content="Copyright BeerAdvocate.com, Inc. All rights reserved." />
+</head>
+
+<body>
+
+
+
+	
+
+<div id="loginBar">
+	<div class="pageWidth">
+		<div class="pageContent">	
+			<h3 id="loginBarHandle">
+				<label for="LoginControl"><a href="login/" class="concealed noOutline">Log in or Sign up</a></label>
+			</h3>
+			
+			<span class="helper"></span>
+
+			
+		</div>
+	</div>
+</div>
+
+
+<div id="headerMover">
+	<div id="headerProxy"></div>
+
+<div id="content" class="">
+	<div class="pageWidth">
+		<div class="pageContent">
+			<!-- main content area -->
+			
+			
+			
+			
+						
+						
+						
+						
+						<div class="breadBoxTop">
+							
+							
+
+<nav>
+	
+
+	<fieldset class="breadcrumb">
+		<a href="misc/quick-navigation-menu" class="OverlayTrigger jumpMenuTrigger" data-cacheOverlay="true" title="Open quick navigation"><!--Jump to...--></a>
+			
+		<div class="boardTitle"><strong>BeerAdvocate Community</strong></div>
+		
+		<span class="crumbs">
+			
+				<span class="crust" itemscope="itemscope" itemtype="http://data-vocabulary.org/Breadcrumb">
+					<a href="http://beeradvocate.com" class="crumb" rel="up" itemprop="url"><span itemprop="title">Home</span></a>
+					<span class="arrow"><span></span></span>
+				</span>
+			
+			
+			
+			
+			
+		</span>
+	</fieldset>
+</nav>
+						</div>
+						
+						
+						
+					
+						
+												
+						
+						
+						
+												
+							<!-- h1 title, description -->
+							<div class="titleBar">
+								
+								<h1>BeerAdvocate Community - Error</h1>
+								
+								
+							</div>
+						
+						
+						
+						
+						
+						<!-- main template -->
+						
+
+
+<div class="errorOverlay">
+	<a class="close OverlayCloser"></a>
+	
+		
+		
+		<div class="baseHtml">
+		
+			<label for="ctrl_0" class="OverlayCloser">The requested page could not be found.</label>
+		
+		</div>
+	
+</div>
+						
+						
+						
+						
+							<!-- login form, to be moved to the upper drop-down -->
+							
+
+
+
+
+
+<form action="login/login" method="post" class="xenForm " id="login" style="display:none">
+
+	
+
+	<div class="ctrlWrapper">
+		<dl class="ctrlUnit">
+			<dt><label for="LoginControl">Your name or email address:</label></dt>
+			<dd><input type="text" name="login" id="LoginControl" class="textCtrl" tabindex="101" /></dd>
+		</dl>
+	
+	
+		<dl class="ctrlUnit">
+			<dt>
+				<label for="ctrl_password">Do you already have an account?</label>
+				<div class="lostPassword"><a href="lost-password/" class="OverlayTrigger OverlayCloser" tabindex="106">Forgot your password?</a></div>
+			</dt>
+			<dd>
+				<ul>
+					<li><label for="ctrl_not_registered"><input type="radio" name="register" value="1" id="ctrl_not_registered" tabindex="105" />
+						No, create an account now.</label></li>
+					<li><label for="ctrl_registered"><input type="radio" name="register" value="0" id="ctrl_registered" tabindex="105" checked="checked" class="Disabler" />
+						Yes, my password is:</label></li>
+					<li id="ctrl_registered_Disabler">
+						<input type="password" name="password" class="textCtrl" id="ctrl_password" tabindex="102" />
+					</li>
+				</ul>
+			</dd>
+		</dl>
+	
+		
+		<dl class="ctrlUnit submitUnit">
+			<dt></dt>
+			<dd>
+				<input type="submit" class="button primary" value="Log in" tabindex="104" data-loginPhrase="Log in" data-signupPhrase="Sign Up" />
+				<label for="ctrl_remember" class="rememberPassword"><input type="checkbox" name="remember" value="1" id="ctrl_remember" tabindex="103" /> Stay logged in</label>
+			</dd>
+		</dl>
+	</div>
+
+	<input type="hidden" name="cookie_check" value="1" />
+	<input type="hidden" name="redirect" value="/community/avatar.php%3Fusername%3DTheJollyHop%26size%3Ds" />
+	<input type="hidden" name="_xfToken" value="" />
+
+</form>
+						
+						
+					
+			
+						
+			<div class="breadBoxBottom">
+
+<nav>
+	
+
+	<fieldset class="breadcrumb">
+		<a href="misc/quick-navigation-menu" class="OverlayTrigger jumpMenuTrigger" data-cacheOverlay="true" title="Open quick navigation"><!--Jump to...--></a>
+			
+		<div class="boardTitle"><strong>BeerAdvocate Community</strong></div>
+		
+		<span class="crumbs">
+			
+				<span class="crust">
+					<a href="http://beeradvocate.com" class="crumb"><span>Home</span></a>
+					<span class="arrow"><span></span></span>
+				</span>
+			
+			
+			
+			
+			
+		</span>
+	</fieldset>
+</nav></div>
+			
+						
+			
+						
+		</div>
+	</div>
+</div>
+
+<header>
+	
+<div id="header">
+	<div id="logoBlock">
+	<div class="pageWidth">
+		<div class="pageContent">
+			
+
+<div style="width:728px; height:90px; float:right; margin:0; padding:0">
+<iframe id="4f99886129932" name="4f99886129932" src="http://ox-d.beeradvocate.com/w/1.0/afr?auid=8782&cb=13791055400" frameBorder="0" frameSpacing="0" scrolling="no" width="728" height="90"><a href="http://ox-d.beeradvocate.com/w/1.0/rc?cs=4f99886129932&cb=13791055400" ><img src="http://ox-d.beeradvocate.com/w/1.0/ai?auid=8782&cs=4f99886129932&cb=13791055400" border="0" alt=""></a></iframe>
+</div>
+			
+			<div id="logo"><a href="http://beeradvocate.com">
+				<span></span>
+				<img src="http://beeradvocate.com/im/beeradvocate-logo-header.png" alt="BeerAdvocate Community" />
+			</a></div>
+			
+			<span class="helper"></span>
+		</div>
+	</div>
+</div>
+	
+
+<div id="navigation" class="pageWidth">
+	<div class="pageContent">
+		<nav>
+
+<div class="navTabs">
+	<ul class="publicTabs">
+	
+		<!-- home -->
+		
+			<li class="navTab home PopupClosed"><a href="http://beeradvocate.com" class="navLink">Home</a></li>
+		
+		
+		
+		<!-- extra tabs: home -->
+		
+		
+		
+		<!-- forums -->
+		
+			<li class="navTab forums Popup PopupControl PopupClosed">
+			
+				<a href="http://beeradvocate.com/community/" class="navLink">Forums</a>
+				<a href="http://beeradvocate.com/community/" class="SplitCtrl" rel="Menu"></a>
+				
+				<div class="Menu JsOnly tabMenu forumsTabLinks">
+					<div class="primaryContent menuHeader">
+						<h3>Forums</h3>
+						<div class="muted">Quick Links</div>
+					</div>
+					<ul class="secondaryContent blockLinksList">
+					
+						
+						<li><a href="search/?type=post">Search Forums</a></li>
+						
+						<li><a href="find-new/threads" rel="nofollow">What's New?</a></li>
+					
+					</ul>
+				</div>
+			</li>
+		
+		
+		
+		<!-- extra tabs: middle -->
+			<li class="navTab PopupClosed"><a href="/beer/" class="navLink">Beers</a></li>
+
+			<li class="navTab PopupClosed"><a href="/beerfly/" class="navLink">Places</a></li>
+
+			<li class="navTab PopupClosed"><a href="/events/calendar/" class="navLink">Events</a></li>
+
+			<li class="navTab PopupClosed"><a href="/mag/" class="navLink">Magazine</a></li>
+		
+		
+		
+
+		
+		
+		<!-- extra tabs: end -->
+		
+		
+		
+		<!-- no selection -->
+		
+			<li class="navTab selected"><div class="tabLinks"></div></li>
+		
+		
+	</ul>
+	
+	
+	
+		<ul class="visitorTabs">
+			<li class="navTab PopupClosed"><a href="login/" class="navLink visitorTabItem">Log in</a></li>
+		</ul>
+	</div>
+
+<span class="helper"></span>
+			
+		</nav>	
+	</div>
+</div>
+	
+
+<div id="searchBar" class="pageWidth">
+	
+	<fieldset id="QuickSearch">
+		<form action="search/search" method="post" class="formPopup">
+			
+			<div class="primaryControls">
+				<!-- block: primaryControls -->
+				<input type="search" name="keywords" value="" class="textCtrl" placeholder="Search..." results="0" title="Enter your search and hit enter" id="QuickSearchQuery" />				
+				<!-- end block: primaryControls -->
+			</div>
+			
+			<div class="secondaryControls">
+				<div class="controlsWrapper">
+				
+					<!-- block: secondaryControls -->
+					<dl class="ctrlUnit">
+						<dt></dt>
+						<dd><ul>
+							<li><label><input type="checkbox" name="title_only" value="1"
+								id="search_bar_title_only" class="AutoChecker"
+								data-uncheck="#search_bar_thread" /> Search titles only</label></li>
+						</ul></dd>
+					</dl>
+				
+					<dl class="ctrlUnit">
+						<dt><label for="searchBar_users">Posted by Member:</label></dt>
+						<dd>
+							<input type="text" name="users" value="" class="textCtrl AutoComplete" id="searchBar_users" />
+							<p class="explain">Separate names with a comma.</p>
+						</dd>
+					</dl>
+				
+					<dl class="ctrlUnit">
+						<dt><label for="searchBar_date">Newer Than:</label></dt>
+						<dd><input type="date" name="date" value="" class="textCtrl" id="searchBar_date" /></dd>
+					</dl>
+					
+					
+				</div>
+				<!-- end block: secondaryControls -->
+				
+				<dl class="ctrlUnit submitUnit">
+					<dt></dt>
+					<dd>
+						<input type="submit" value="Search" class="button primary Tooltip" title="Find Now" />
+						<div class="Popup" id="commonSearches">
+							<a rel="Menu" class="button NoPopupGadget Tooltip" title="Useful Searches" data-tipclass="flipped"><span class="arrowWidget"></span></a>
+							<div class="Menu">
+								<div class="primaryContent menuHeader">
+									<h3>Useful Searches</h3>
+								</div>
+								<ul class="secondaryContent blockLinksList">
+									<!-- block: useful_searches -->
+									<li><a href="find-new/threads?recent=1" rel="nofollow">Recent Threads</a></li>
+									
+									<!-- end block: useful_searches -->
+								</ul>
+							</div>
+						</div>
+						<a href="search/" class="button moreOptions Tooltip" title="Advanced search">More...</a>
+					</dd>
+				</dl>
+				
+			</div>
+			
+			<input type="hidden" name="_xfToken" value="" />
+		</form>		
+	</fieldset>
+	
+</div>
+</div>
+
+	
+	
+</header>
+
+</div>
+
+<footer>
+	
+
+
+<div class="footer">
+	<div class="pageWidth">
+		<div class="pageContent">
+			
+			<ul class="footerLinks">
+				<li><a href="/contact">Contact</a></li>
+				<li><a href="/community/help/terms">Terms</a></li>
+				<li><a href="/">Home</a></li>
+				<li><a href="/community/avatar.php%3Fusername%3DTheJollyHop%26size%3Ds#navigation">Top</a></li>
+				<li><a href="forums/-/index.rss" rel="alternate" class="globalFeed" target="_blank"
+					title="RSS feed for BeerAdvocate Community">RSS</a></li>
+			</ul>
+			
+			<span class="helper"></span>
+		</div>
+	</div>
+</div>
+
+<div class="footerLegal">
+	<div class="pageWidth">
+		<div class="pageContent">
+			<div id="copyright">Copyright &copy; BeerAdvocate. All rights reserved. Respect Beer.</div>
+			
+
+			<span class="helper"></span>
+		</div>
+	</div>	
+</div>
+
+</footer>
+
+<script>
+
+
+jQuery.extend(true, XenForo,
+{
+	visitor: { user_id: 0 },
+	serverTimeInfo:
+	{
+		now: 1379105540,
+		today: 1379026800,
+		todayDow: 5
+	},
+	_lightBoxUniversal: "0",
+	_enableOverlays: "1",
+	_animationSpeedMultiplier: "1",
+	_overlayConfig:
+	{
+		top: "10%",
+		speed: 200,
+		closeSpeed: 100,
+		mask:
+		{
+			color: "rgb(255, 255, 255)",
+			opacity: "0.6",
+			loadSpeed: 200,
+			closeSpeed: 100
+		}
+	},
+	_ignoredUsers: [],
+	_loadedScripts: {"login_bar":true},
+	_cookieConfig: { path: "/", domain: "", prefix: "xf_"},
+	_csrfToken: "",
+	_csrfRefreshUrl: "login/csrf-token-refresh",
+	_jsVersion: "1d8d27b3"
+});
+jQuery.extend(XenForo.phrases,
+{
+	cancel: "Cancel",
+
+	a_moment_ago:    "A moment ago",
+	one_minute_ago:  "1 minute ago",
+	x_minutes_ago:   "%minutes% minutes ago",
+	today_at_x:      "Today at %time%",
+	yesterday_at_x:  "Yesterday at %time%",
+	day_x_at_time_y: "%day% at %time%",
+
+	day0: "Sunday",
+	day1: "Monday",
+	day2: "Tuesday",
+	day3: "Wednesday",
+	day4: "Thursday",
+	day5: "Friday",
+	day6: "Saturday",
+
+	_months: "January,February,March,April,May,June,July,August,September,October,November,December",
+	_daysShort: "Sun,Mon,Tue,Wed,Thu,Fri,Sat",
+
+	following_error_occurred: "The following error occurred",
+	server_did_not_respond_in_time_try_again: "The server did not respond in time. Please try again.",
+	logging_in: "Logging in",
+	click_image_show_full_size_version: "Click this image to show the full-size version.",
+	show_hidden_content_by_x: "Show hidden content by {names}"
+});
+
+// Facebook Javascript SDK
+XenForo.Facebook.appId = "";
+XenForo.Facebook.forceInit = false;
+
+
+</script>
+
+
+
+
+</body>
+</html>
